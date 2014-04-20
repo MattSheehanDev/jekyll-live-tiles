7fd3459ed4c4cf5139e289f375f411fe5f49d93e
Matt Sheehan <saltykracka@gmail.com>
2014-04-20 18:15:39 -0400

    fixed date formatting

--- jekyll-live-tiles.rb
+++ jekyll-live-tiles.rb
@@ -105,7 +105,7 @@ module Jekyll
             v.binding("template"=>"TileSquare310x310TextList02", "branding"=>"logo", "fallback"=>"TileWideText09") do |b|
               b.tag!("text", post.title, "id"=>"1")
               b.tag!("text", shorten(strip(post.content)),"id"=>"2")
-              b.tag!("text", post.date, "id"=>"3")
+              b.tag!("text", "#{post.date.month}-#{post.date.day}-#{post.date.year}", "id"=>"3")
             end
           end
         end
